aaa223e51dab80633933d7f2b9472b3613078ea8
Marcelo Pereira <marcelo96jr@gmail.com>
2022-04-19 23:53:03 -0300

    test: type inputs and submit form

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,8 +1,21 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function () {
-  it('verifica o título da aplicação', function () {
+  beforeEach(function () {
     cy.visit('./src/index.html');
+  });
+  it('verifica o título da aplicação', function () {
     cy.title().should('eq', 'Central de Atendimento ao Cliente TAT');
+  });
+
+  it.only('preenche os campos obrigatórios e envia o formulário', function () {
+    cy.get('#firstName').type('Marcelo');
+    cy.get('#lastName').type('Pereira');
+    cy.get('#email').type('marcelopereiradev@gmail.com');
+    cy.get('#phone').type(85988888888);
+    cy.get('#open-text-area').type('Teste');
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.success').should('be.visible');
   });
 });
