3c3cb21be5966e6a14ca901f1dd787152754724e
Marcelo Pereira <marcelo96jr@gmail.com>
2022-04-19 23:59:23 -0300

    test: change delay on type

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -9,11 +9,13 @@ describe('Central de Atendimento ao Cliente TAT', function () {
   });
 
   it.only('preenche os campos obrigatórios e envia o formulário', function () {
+    const longText = 'Teste teste teste teste teste teste teste teste';
+
     cy.get('#firstName').type('Marcelo');
     cy.get('#lastName').type('Pereira');
     cy.get('#email').type('marcelopereiradev@gmail.com');
     cy.get('#phone').type(85988888888);
-    cy.get('#open-text-area').type('Teste');
+    cy.get('#open-text-area').type(longText, { delay: 0 });
     cy.get('button[type="submit"]').click();
 
     cy.get('.success').should('be.visible');
