1838ea7c71039dc0827a3dfb79b2cbf7df7c2f1a
Marcelo Pereira <marcelo96jr@gmail.com>
2022-04-21 22:38:36 -0300

    test: phone input accept only numbers

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -31,4 +31,8 @@ describe('Central de Atendimento ao Cliente TAT', function () {
 
     cy.get('.error').should('be.visible');
   });
+
+  it.only('campo telefone continua vazio quando preenchido com valor não numérico', function () {
+    cy.get('#phone').type('abcdefghij').should('have.value', '');
+  });
 });
