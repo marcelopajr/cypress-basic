bc5bcb7bd5555c8f93584bb451bbfbc342c21fa3
Marcelo Pereira <marcelo96jr@gmail.com>
2022-04-21 23:02:03 -0300

    test: error when submit without fill the form

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -47,7 +47,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     cy.get('.error').should('be.visible');
   });
 
-  it.only('preenche e limpa os campos nome, sobrenome, email e telefone', function () {
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', function () {
     cy.get('#firstName')
       .type('Marcelo')
       .should('have.value', 'Marcelo')
@@ -68,5 +68,11 @@ describe('Central de Atendimento ao Cliente TAT', function () {
       .should('have.value', '85988888888')
       .clear()
       .should('have.value', '');
+  });
+
+  it.only('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function () {
+    cy.get('button[type="submit"]').click();
+
+    cy.get('.error').should('be.visible');
   });
 });
