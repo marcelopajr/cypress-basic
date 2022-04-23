bbc4abc7c2c838a32bad88d47e653f375175abde
Marcelo Pereira <marcelo96jr@gmail.com>
2022-04-23 17:21:11 -0300

    test: wrap and check each of radio buttons

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -94,9 +94,18 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     cy.get('#product').select(1).should('have.value', 'blog');
   });
 
-  it.only('marca o tipo de atendimento "Feedback"', function () {
+  it('marca o tipo de atendimento "Feedback"', function () {
     cy.get('input[type="radio"][value="feedback"]')
       .check()
       .should('have.value', 'feedback');
+  });
+
+  it.only('marca cada tipo de atendimento"', function () {
+    cy.get('input[type="radio"]')
+      .should('have.length', 3)
+      .each(function ($radio) {
+        cy.wrap($radio).check();
+        cy.wrap($radio).should('be.checked');
+      });
   });
 });
