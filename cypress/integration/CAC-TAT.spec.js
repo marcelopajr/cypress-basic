1e01d326c4e12447da5f32e67f2098fef00e8cb4
Marcelo Pereira <marcelo96jr@gmail.com>
2022-04-23 17:04:39 -0300

    test: select by value

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -82,7 +82,11 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     cy.get('.success').should('be.visible');
   });
 
-  it.only('seleciona um produto (YouTube) por seu texto', function () {
+  it('seleciona um produto (YouTube) por seu texto', function () {
     cy.get('#product').select('YouTube').should('have.value', 'youtube');
+  });
+
+  it.only('seleciona um produto (Mentoria) por seu valor (value)', function () {
+    cy.get('#product').select('mentoria').should('have.value', 'mentoria');
   });
 });
