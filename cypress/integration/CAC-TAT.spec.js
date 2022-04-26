2fe8c6473d6ffe22aa6c9503e0d937d2a2c76514
Marcelo Pereira <marcelo96jr@gmail.com>
2022-04-25 21:05:03 -0300

    test: upload with drag-drop

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -118,10 +118,19 @@ describe('Central de Atendimento ao Cliente TAT', function () {
       .should('not.be.checked');
   });
 
-  it.only('seleciona um arquivo da pasta fixtures', function () {
+  it('seleciona um arquivo da pasta fixtures', function () {
     cy.get('#file-upload')
       .should('not.have.value')
       .selectFile('./cypress/fixtures/example.json')
+      .should(function (input) {
+        expect(input[0].files[0].name).to.equal('example.json');
+      });
+  });
+
+  it.only('seleciona um arquivo simulando um drag-and-drop', function () {
+    cy.get('#file-upload')
+      .should('not.have.value')
+      .selectFile('./cypress/fixtures/example.json', { action: 'drag-drop' })
       .should(function (input) {
         expect(input[0].files[0].name).to.equal('example.json');
       });
