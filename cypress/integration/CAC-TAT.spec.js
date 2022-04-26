a705b1a8e8aba1d61856dd050a7b22f31fb57c08
Marcelo Pereira <marcelo96jr@gmail.com>
2022-04-25 21:41:05 -0300

    test: link with target _blank

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -136,12 +136,16 @@ describe('Central de Atendimento ao Cliente TAT', function () {
       });
   });
 
-  it.only('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function () {
+  it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function () {
     cy.fixture('example.json').as('sampleFile');
     cy.get('input[type="file"]')
       .selectFile('@sampleFile')
       .should(function (input) {
         expect(input[0].files[0].name).to.equal('example.json');
       });
+  });
+
+  it.only('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function () {
+    cy.get('#privacy a').should('have.attr', 'target', '_blank');
   });
 });
