b7cbb4e4d8b269e381027a7ae81aa4217a629025
Marcelo Pereira <marcelo96jr@gmail.com>
2022-04-25 21:34:25 -0300

    test: upload with alias

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -127,10 +127,19 @@ describe('Central de Atendimento ao Cliente TAT', function () {
       });
   });
 
-  it.only('seleciona um arquivo simulando um drag-and-drop', function () {
+  it('seleciona um arquivo simulando um drag-and-drop', function () {
     cy.get('#file-upload')
       .should('not.have.value')
       .selectFile('./cypress/fixtures/example.json', { action: 'drag-drop' })
+      .should(function (input) {
+        expect(input[0].files[0].name).to.equal('example.json');
+      });
+  });
+
+  it.only('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function () {
+    cy.fixture('example.json').as('sampleFile');
+    cy.get('input[type="file"]')
+      .selectFile('@sampleFile')
       .should(function (input) {
         expect(input[0].files[0].name).to.equal('example.json');
       });
